7e4074f1bb1954d3753816088cadda19e22a6612
Aleksey <61495393+VershkovAleksey@users.noreply.github.com>
2026-04-02 10:37:50 +0300

    Update marking.service.js

--- src/services/marking.service.js
+++ src/services/marking.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://130.193.52.139/api/Marking/";
+const API_URL = "http://213.165.218.221/api/Marking/";
 
 class MarkingService {
   sendSetsToMark(request) {
