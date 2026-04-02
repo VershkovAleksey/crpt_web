08abfe4e167cd3f139111a2723ddc81eff33b82a
AVershkov <AVershkov@inno.tech>
2026-04-02 11:27:25 +0300

    fix packagelock

--- src/services/crpt.token.service.js
+++ src/services/crpt.token.service.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-import authHeader from "./auth-header";
+import axios from "axios"
+import authHeader from "./auth-header"
 
 const API_URL = "http://213.165.218.221/api/Auth/";
 
